9d0cd1986dbb7501ebfab0b8c5167ed3ebaa6b3b
Yasin Satvati <57200251+theashozosht@users.noreply.github.com>
2024-07-28 13:42:19 +0430

    semi fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
-import { CalendarModule } from "primeng/calendar";
+import { CalendarModule } from 'primeng/calendar';
 import { DialogModule } from 'primeng/dialog';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 import { ConfirmPopupModule } from 'primeng/confirmpopup';
-import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -23,54 +28,74 @@ import { BrowserModule } from '@angular/platform-browser';
     DialogModule,
     ConfirmDialogModule,
     ConfirmPopupModule,
-    ToastModule
+    ToastModule,
   ],
-  providers: [
-    ConfirmationService,
-    MessageService
-  ],
+  providers: [ConfirmationService, MessageService],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  private confirmationService = inject(ConfirmationService)
-  private messageService = inject(MessageService)
+  private confirmationService = inject(ConfirmationService);
+  private messageService = inject(MessageService);
   passengerForm = new FormGroup({
-    passengerArray: new FormArray([new FormGroup({
-      passengerName: new FormControl('', [Validators.required]),
-      price: new FormControl<number>(0, [Validators.required]),
-      roomNo: new FormControl<number>(0, [Validators.required]),
-      providerName: new FormControl('', [Validators.required]),
-      passengerId: new FormControl<number>(0, [Validators.required]),
-      fromDate: new FormControl('', [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)]),
-      toDate: new FormControl('', [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)])
-    })])
-  })
+    passengerArray: new FormArray([
+      new FormGroup({
+        passengerName: new FormControl('', [Validators.required]),
+        price: new FormControl<number>(0, [Validators.required]),
+        roomNo: new FormControl<number>(0, [Validators.required]),
+        providerName: new FormControl('', [Validators.required]),
+        passengerId: new FormControl<number>(0, [Validators.required]),
+        fromDate: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^\d{4}-\d{2}-\d{2}$/),
+        ]),
+        toDate: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^\d{4}-\d{2}-\d{2}$/),
+        ]),
+      }),
+    ]),
+  });
 
   confirm(event: Event) {
     if (this.passengerForm.errors !== null) {
-      this.messageService.add({ severity: 'error', summary: 'نادرست', detail: 'اطلاعات وارد شده نادرست می‌باشد' })
+      this.messageService.add({
+        severity: 'error',
+        summary: 'نادرست',
+        detail: 'اطلاعات وارد شده نادرست می‌باشد',
+      });
       return;
     }
     this.confirmationService.confirm({
       key: 'confirmForm',
-      target: event.target || new EventTarget,
+      target: event.target || new EventTarget(),
       message: 'از صحت اطلاعات وارد شده مطمئن هستید ؟',
       icon: 'pi pi-exclamation-triangle',
       acceptLabel: 'بلی',
       rejectLabel: 'خیر',
       accept: () => {
-        this.sendToApi()
+        this.sendToApi();
         // this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
       },
-      reject: () => { }
+      reject: () => {},
     });
   }
 
   sendToApi(): void {
-    for (let i = 0; i < this.passengerForm.controls.passengerArray.controls.length; i++) {
-      const { passengerName, price, roomNo, providerName, passengerId, fromDate, toDate } =
-        this.passengerForm.controls.passengerArray.controls[i].value;
+    for (
+      let i = 0;
+      i < this.passengerForm.controls.passengerArray.controls.length;
+      i++
+    ) {
+      const {
+        passengerName,
+        price,
+        roomNo,
+        providerName,
+        passengerId,
+        fromDate,
+        toDate,
+      } = this.passengerForm.controls.passengerArray.controls[i].value;
     }
   }
 }
